da3e9936d4078d842e3975327dd306176258f37f
harrisonworks <harrisonadmcewen@gmail.com>
2022-02-14 13:08:23 +1100

    got rid of pesky store filters

--- store/index.js
+++ store/index.js
@@ -53,54 +53,55 @@ export const actions = {
   },
 
   async fetchAllListings({ commit }) {
-    if (process.env.NODE_ENV !== 'production') {
-      const result = await fetch(`http://localhost:3000/wanted.json`).then(
-        (res) => res.json()
-      )
+    // if (process.env.NODE_ENV !== 'production') {
+    //   const result = await fetch(`http://localhost:3000/wanted.json`).then(
+    //     (res) => res.json()
+    //   )
 
-      return new Promise((resolve) => {
-        commit('updateListings', result)
-        console.log('major query complete')
-        resolve(result)
-      })
-    } else {
-      let arrResult = []
+    //   return new Promise((resolve) => {
+    //     commit('updateListings', result)
+    //     console.log('major query complete')
+    //     resolve(result)
+    //   })
+    // } else {
+    let arrResult = []
 
-      let filtered = null
-      let result = []
-      let page = 1
-      let started = false
-      let toContinue = true
+    let filtered = null
+    let result = []
+    let page = 1
+    let started = false
+    let toContinue = true
 
-      while (toContinue) {
-        if (started === false || result.items.length === 20) {
-          result = await fetch(`${siteURL}?page=${page}`).then((res) =>
-            res.json()
-          )
+    while (toContinue) {
+      if (started === false || result.items.length === 20) {
+        result = await fetch(`${siteURL}?page=${page}`).then((res) =>
+          res.json()
+        )
 
-          started = true
-          page++
+        started = true
+        page++
 
-          // no victims
-          filtered = await result.items.filter((person) => {
-            const crimeList = crimeSorter(person.description)
-            // const criminalList = !victimCheck([...person.subjects, ...crimeList])
+        // DO THE FILTERS ON THE FRONT FOOL
+        // no victims
+        // filtered = await result.items.filter((person) => {
+        //   const crimeList = crimeSorter(person.description)
+        //   // const criminalList = !victimCheck([...person.subjects, ...crimeList])
 
-            return cyberCheck([...person.subjects, ...crimeList])
-          })
+        //   return cyberCheck([...person.subjects, ...crimeList])
+        // })
 
-          console.log('loaded page', result.items.length)
-          commit('updatePage', 1)
-        } else if (result.items.length < 20) {
-          toContinue = false
-        }
-        arrResult = arrResult.concat(filtered)
+        console.log('loaded page', result.items.length)
+        commit('updatePage', 1)
+      } else if (result.items.length < 20) {
+        toContinue = false
       }
-      return new Promise((resolve) => {
-        commit('updateListings', arrResult)
-        console.log('major query complete')
-        resolve(arrResult)
-      })
+      arrResult = arrResult.concat(filtered)
     }
+    return new Promise((resolve) => {
+      commit('updateListings', arrResult)
+      console.log('major query complete')
+      resolve(arrResult)
+    })
+    // }
   },
 }
